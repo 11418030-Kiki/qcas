fe934e725cd883cd11605568b55c84c515eceaff
rajeevraibhatia <rajeevraibhatia@172.29.81.13>
2016-12-04 10:32:00 +1030

    Changes Done

--- src/qcas/QCAS.java
+++ src/qcas/QCAS.java
@@ -21,7 +21,7 @@ public class QCAS extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Demo.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
         Scene scene = new Scene(root, 630, 450);
         stage.setTitle("Home Screen");
         stage.setScene(scene);
